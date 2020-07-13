6f2f01e0d19c789038281c8e7c76d8b5b7d3b1e3
serpiara <sergio.pizarroo@outlook.com>
2020-07-13 00:37:48 -0400

    Mayor de 3 Done

---
+++ mayor_de_3.rb
@@ -0,0 +1,15 @@
+n1 = ARGV[0].to_i
+n2 = ARGV[1].to_i
+n3 = ARGV[2].to_i
+
+if n1 > n2 
+    if n1 > n3
+        puts n1
+    else
+        puts n3
+    end       
+elsif n2 > n3
+    puts n2
+else
+    puts n3
+end
